b61069e793c0a393df4797e34d281ed10f153880
Alannah <n00220608@iadt.ie>
2023-10-17 13:23:34 +0100

    First Commit

---
+++ database/seeders/SongsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+use App\Models\Song;
+
+class SongsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        Song::factory(20)->create();
+    }
+}
