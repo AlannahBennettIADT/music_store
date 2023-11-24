c07282dfff02eabaecc18c884ea0ccb14a79ac28
Alannah <n00220608@iadt.ie>
2023-11-24 13:43:02 +0000

    Fixed User Album Controller

--- app/Http/Controllers/user/AlbumController.php
+++ app/Http/Controllers/user/AlbumController.php
@@ -13,14 +13,29 @@ class AlbumController extends Controller
     public function index()
     {
         //
+        $user = Auth::user();
+        $user->authorizeRoles('user');
+        
+        $albums = Album::all();
+        return view('user.albums.index')->with('albums',$albums);
     }
 
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Album $album)
     {
         //
+        $user = Auth::user();
+        $user->authorizeRoles('user');
+
+        if(!Auth::id()){
+            return abort(403);
+        }
+
+        $songs = $album->songs;
+
+        return view('user.albums.show',compact('album','songs'));
     }
 }
